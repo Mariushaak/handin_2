126b925b2e1f3ca2d8db32df864cce212ef8413d
Marius <36195973+Mariushaak@users.noreply.github.com>
2018-02-13 13:48:30 +0100

    hm

--- Tetris.cpp
+++ Tetris.cpp
@@ -4,14 +4,14 @@
 
 int main() {
 
-	sf::RenderWindow window(sf::VideoMode(600, 400), "Tetris game");
+	sf::RenderWindow window(sf::VideoMode(1200, 800), "Tetris game");
 	sf::RectangleShape squares;
-	squares.setSize(sf::Vector2f(40, 40));
+	squares.setSize(sf::Vector2f(80, 80));
 	squares.setFillColor(sf::Color::Cyan);
-	
+	sf::Clock time;
 
-	int squareX = 280;
-	int squareY = 180;
+	int squareX = 0;
+	int squareY = 0;
 
 	while (window.isOpen()) {
 		sf::Event event;
@@ -19,13 +19,15 @@ int main() {
 			if (event.type == sf::Event::Closed)
 				window.close();
 			if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Left) && (squareX > 0))
-				squareX -= 10;
-			if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Right) && (squareX < 560))
-				squareX += 10;
+				squareX -= 80;
+			if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::Right) && (squareX < 1120))
+				squareX += 80;
 		}
 
 		squares.setPosition(squareX, squareY);
-		
+	
+		if(squareY < 720)
+		squareY = 80 * int (time.getElapsedTime().asSeconds());
 		
 		//DRAW :
 
